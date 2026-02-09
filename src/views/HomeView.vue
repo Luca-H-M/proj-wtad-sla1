c9db218c7260e40091275393b98b662ee8aafde6
Luca-H-M <h.luca@escolar.ifrn.edu.br>
2026-02-09 18:42:04 -0300

    terminei o homeview eu acho, eu n vou conseguir terminar o resto provavelmente

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <figure>
-    <img src="../assets/logo.png" alt="pizza">
+    <img src="../assets/pizza1.png" alt="pizza">
   </figure>
   <body>
     <h1>Pizzaria Glutoza</h1>
@@ -13,6 +13,11 @@
           <li><router-link to="/locais">Locais</router-link></li>
           <li><router-link to="/pizzas">Pizzas mais famosas</router-link></li>
       </ul>
+      <ol>
+        <li><img src="../assets/pizza1.png" alt="pizza"></li>
+        <li><img src="../assets/pizza1.png" alt="pizza"></li>
+        <li><img src="../assets/pizza1.png" alt="pizza"></li>
+      </ol>
     </nav>
   </body>
   
@@ -24,6 +29,7 @@
     position: absolute;
     width: 100%;
     max-height: 3rem;
+    z-index: -1;
   };
   h1{
     border-radius: 10px;
@@ -34,5 +40,24 @@
   p{
     margin: 3rem;
     border-radius: 30px;
+    background-color: white;
+  }
+  ul{
+    display: flex;
+    list-style-type: none;
+    justify-content: space-between;
+    li{
+      border-radius: 5rem;
+      margin: auto;
+      background-color: gray;
+    }
+  }
+  ol{
+    display: flex;
+    list-style-type: none;
+    justify-content: space-between;
+    li{
+      max-width: 10rem;
+    }
   }
 </style>
